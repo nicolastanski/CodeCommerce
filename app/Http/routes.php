0a3f71097721769ef4e472f9cba8c2482bfcc97a
Nicolas Tanski <nicolas.tanski@gmail.com>
2015-10-07 23:13:34 -0300

    Ajustes Criação de Rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,32 +31,9 @@ Route::get('admin/categories/excluir', ['as' => 'excluirCategoria', 'uses' => 'A
 
 //Rotas Produtos
 Route::get('admin/products', 'AdminProductsController@listar');
-Route::get('admin/products', 'AdminProductsController@index');
 Route::get('admin/products/adicionar', ['as' => 'adicionarProduto', 'uses' => 'AdminProductsController@adicionar']);
 Route::get('admin/products/editar/{id?}', [
     'as' => 'editarProduto',
     'uses' => 'AdminProductsController@editar'
 ]);
 Route::get('admin/products/excluir', ['as' => 'excluirProduto', 'uses' => 'AdminProductsController@excluir']);
-
-
-=======
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('exemplo','Exemplo@index');
-
-Route::get('admin/categories','AdminCategoriesController@index');
-Route::get('admin/products','AdminProductsController@index');
->>>>>>> 4f9741e1b0cb1fa82265640e60dcf042eb39454c
